88a110a0e9115e08e6ed06225ec373834fc3921e
Zhenyu,Liu <liuzhenyulive@live.com>
2019-01-10 23:21:20 +0800

    add unit test

---
+++ UnitTest/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void Test ()
+        {
+            Console.WriteLine("This is the first unit test");
+        }
+    }
+}
